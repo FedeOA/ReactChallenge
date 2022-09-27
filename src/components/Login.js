05ae0c42274a6ef62238b92faa8d9c84325fca22
FedeOA <fedeacosta07@hotmail.com>
2022-09-27 11:02:17 -0300

    se agregaron validaciones

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,41 @@
 function Login(){
 
+    const submitHandler = e =>{
+       
+        e.preventDefault();
+       
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        
+        if(email===''|| password===''){
+            
+            console.log("los campos no pueden estar vacios");
+            return;
+        }
+
+        if (email !== '' &&  !regexEmail.test(email)){
+
+            console.log("Debe escribir una direccion de correo electronico valida")
+            return;
+        }
+
+        if (email !== 'challenge@alkemy.org'||password!=='react'){
+            console.log("credenciales invalidas");
+            return;
+        }
+        
+        console.log("Ok. Estamos listos para enviar la informacion");
+
+    }
+
     return (
         <>
             <h2>Formulario de Login</h2>
-            <form>
+            <form onSubmit={submitHandler}>
                 <label>
                     <span>Correo electronico:</span> <br/>
-                    <input type= "email" name="email" />
+                    <input type= "text" name="email" />
                 </label>
                 <br/>
                 <label>

874a31895a3d7554f7f99b038e334ef0b6ffa55f
FedeOA <fedeacosta07@hotmail.com>
2022-10-14 21:45:28 -0300

    add comment section in news

---
+++ src/Commentt.js
@@ -0,0 +1,45 @@
+function Commentt() {
+  return (
+    <div className="btn-toolbar" role="toolbar">
+      <div className="container">
+        <section class=" _aaoe _ae5y">
+          <div class="_aaof">
+            <form class="_aao9" method="POST">
+              <div class="_aaoa">
+                <div>
+                  <button className="_abl- _abm2" type="button">
+                    <div className="_abm0">
+                      <svg
+                        aria-label="Emoticono"
+                        class="_ab6-"
+                        color="#262626"
+                        fill="#262626"
+                        height="24"
+                        role="img"
+                        viewBox="0 0 24 24"
+                        width="24"
+                      >
+                        <path d="M15.83 10.997a1.167 1.167 0 1 0 1.167 1.167 1.167 1.167 0 0 0-1.167-1.167Zm-6.5 1.167a1.167 1.167 0 1 0-1.166 1.167 1.167 1.167 0 0 0 1.166-1.167Zm5.163 3.24a3.406 3.406 0 0 1-4.982.007 1 1 0 1 0-1.557 1.256 5.397 5.397 0 0 0 8.09 0 1 1 0 0 0-1.55-1.263ZM12 .503a11.5 11.5 0 1 0 11.5 11.5A11.513 11.513 0 0 0 12 .503Zm0 21a9.5 9.5 0 1 1 9.5-9.5 9.51 9.51 0 0 1-9.5 9.5Z"></path>
+                      </svg>
+                    </div>
+                  </button>
+                </div>
+              </div>
+              <textarea
+                aria-label="Añade un comentario..."
+                placeholder="Añade un comentario..."
+                class="_ablz _aaoc"
+                autocomplete="off"
+                autocorrect="off"
+              ></textarea>
+              <button class="_acan _acao _acas" disabled="" type="submit">
+                <div class="_aacl _aaco _aacw _aad0 _aad6 _aade">Publicar</div>
+              </button>
+            </form>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
+export default Commentt;
--- src/components/List.js
+++ src/components/List.js
@@ -1,76 +1,130 @@
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import { Container} from 'react-bootstrap';
-import {useState , useEffect} from "react";
-import axios from 'axios';
-import {Link} from 'react-router-dom'
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import { Container } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "../App.css";
+import Commentt from "../Commentt";
 
-function List(){
+function List() {
+  let token = localStorage.getItem("token");
 
-    let token=localStorage.getItem('token');
+  const [newList, setNewList] = useState([]);
 
-    const [newList, setNewList] = useState([]);
+  const[comment,setComment]=useState(false);
 
-    const instance = axios.create({
-        baseURL: 'http://localhost:8080/v1',
-        timeout: 1000,
-        headers: {'Authorization': 'Bearer '+token}
+  const handleComment = ()=>{
+
+    setComment(!comment);
+
+  }
+
+
+  const instance = axios.create({
+    baseURL: "http://localhost:8080/v1",
+    timeout: 1000,
+    headers: { Authorization: "Bearer " + token },
+  });
+
+  useEffect(() => {
+    instance.get("/news").then((response) => {
+      const apiData = response.data.content;
+      setNewList(apiData);
+      console.log(apiData);
+      setComment(false);
     });
-      
-    
-    useEffect(() => {
-       
-        instance.get('/news')
-            .then(response=>{
-                const apiData=response.data.content;
-                setNewList(apiData);
-                console.log(apiData);
-           })
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[setNewList]);
-    
-    
-    return (
-        <>
-        
-            <Container className='m-box child h300 col-sm-8 col-xs-12 pl-pr' >
-            <br></br>
-            <br></br>
-                    <Row xs={1} md={2} className="g-4" style={{position:""}} >
-                        
-                    {
-                       newList.map((oneNew,idx)=>{   
-                            return(
-                            
-                                <Col key={idx} style={{width:"320px"}} >
-                                    <Card style={{columnCount:"3"}}>
-                                    <Card.Title ><p style={{display:"flex",justifyContent:"center",fontSize:"30px",fontWeight:"600",fontFamily:"Serif"}}>{oneNew.name}</p></Card.Title>
-                                    <Card.Img variant="top" src="https://cohorte-agosto-38d749a7.s3.amazonaws.com/1661519321851-LOGO-SOMOS_MAS.png" />
-                                    <Card.Body>
-                                        
-                                        
-                                        <div className="btn-toolbar" role="toolbar">
-                                            <div className="btn-group" role="group">
-                                            <Link to={`/update?newId=${oneNew.id}`} className="btn btn-primary">Update</Link>
-                                            </div>
-                                            <div className="col-sm-1 col-xs-1 col-md-1 col-lg-1"></div>
-                                            <div className="btn-group" role="group">
-                                                <Link to={`/detalle?newId=${oneNew.id}`} className="btn btn-primary">Detail</Link>
-                                            </div>
-                                        </div>
-                                    </Card.Body>
-                                    </Card>
-                                </Col>
-                            )
-                        })    
-                    }  
-                    </Row>      
-                            
-                </Container>
+  }, [setNewList]);
+
+  return (
+    <>
+      <Container className="m-box child h300 col-sm-8 col-xs-12 pl-pr">
+        <br></br>
+        <br></br>
+        <Row xs={1} md={2} className="g-4" style={{ marginLeft:"150px"}}>
+          {newList.map((oneNew, idx) => {
+            return (
+              <Col key={idx} style={{ width: "700px" }}>
+                <Card style={{ columnCount: "2" }}>
+                  <Card.Title>
+                    <p
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        fontSize: "30px",
+                        fontWeight: "600",
+                        fontFamily: "Serif",
+                      }}
+                    >
+                      {oneNew.name}
+                    </p>
+                  </Card.Title>
+                  <Card.Img
+                    variant="top"
+                    src="https://cohorte-agosto-38d749a7.s3.amazonaws.com/1661519321851-LOGO-SOMOS_MAS.png"
+                  />
+                  <p style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        fontSize: "30px",
+                        fontWeight: "600",
+                        fontFamily: "Serif",
+                      }}>{oneNew.content}</p>
+                  <Card.Body>
+                    <button className="_abl-" type="button" onClick={handleComment}>
+                      <div className="_abm0 _abm1">
+                        <svg
+                          aria-label="Comentar"
+                          className="_ab6-"
+                          color="#8e8e8e"
+                          fill="#8e8e8e"
+                          height="24"
+                          role="img"
+                          viewBox="0 0 24 24"
+                          width="24"
+                        >
+                          <path
+                            d="M20.656 17.008a9.993 9.993 0 1 0-3.59 3.615L22 22Z"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                          ></path>
+                        </svg>
+                      </div>
+                      <div className="_abm0 _abl_">
+                        <svg
+                          aria-label="Comentar"
+                          class="_ab6-"
+                          color="#262626"
+                          fill="#262626"
+                          height="24"
+                          role="img"
+                          viewBox="0 0 24 24"
+                          width="24"
+                        >
+                          <path
+                            d="M20.656 17.008a9.993 9.993 0 1 0-3.59 3.615L22 22Z"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                          ></path>
+                        </svg>
+                      </div>
+                    </button>
+                    {comment?<Commentt/>:undefined}
+                </Card.Body>
+                </Card>
+              </Col>
+            );
             
-        
-        </>
-    )
+          })}
+        </Row>
+      </Container>
+      <br></br>
+    </>
+  );
 }
 export default List;
